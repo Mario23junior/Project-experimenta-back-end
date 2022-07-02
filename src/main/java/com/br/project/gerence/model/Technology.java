cf74d5584e4d4cb57e6d8acbb21b070e50901743
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-07-02 20:10:08 -0300

    Criando modelo de dados

---
+++ src/main/java/com/br/project/gerence/model/Technology.java
@@ -0,0 +1,46 @@
+package com.br.project.gerence.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Technology {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(length = 200,nullable = false)
+	private String name;
+
+	@Column(length = 30, nullable = false)
+	private String category;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+}
