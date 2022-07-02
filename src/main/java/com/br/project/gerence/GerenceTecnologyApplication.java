63ecaf6d404636f4f1dd1a5535fbcfba6820e639
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-07-02 18:04:11 -0300

    Iniciando projeto de gerencia de tecnologias

---
+++ src/main/java/com/br/project/gerence/GerenceTecnologyApplication.java
@@ -0,0 +1,13 @@
+package com.br.project.gerence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenceTecnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenceTecnologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/project/gerence/GerenceTecnologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.project.gerence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenceTecnologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
