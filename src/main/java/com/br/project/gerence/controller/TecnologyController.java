b31ccdee928f1451eebb93274a2547d85b8e6cb6
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-07-03 04:13:13 -0300

    Configurando retorno de dados

--- src/main/java/com/br/project/gerence/controller/TecnologyController.java
+++ src/main/java/com/br/project/gerence/controller/TecnologyController.java
@@ -3,17 +3,24 @@ package com.br.project.gerence.controller;
 import java.util.List;
 
 import com.br.project.gerence.model.Technology;
+import com.br.project.gerence.repository.RepositoryTecnology;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/technology/")
+@RequestMapping("/api/technology")
 public class TecnologyController {
    
+	private RepositoryTecnology repository;
+	
+	public TecnologyController(RepositoryTecnology repository) {
+		this.repository = repository;
+	}
+	
 	@GetMapping
 	public List<Technology> list(){
-		return null;
+		return repository.findAll();
 	}
 }
