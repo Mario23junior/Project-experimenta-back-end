976578069fe6005d540ff4ac98240e6d356b680c
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-07-04 12:54:16 -0300

    refatorando inclucao no banco de dados

--- src/main/java/com/br/project/gerence/controller/TecnologyController.java
+++ src/main/java/com/br/project/gerence/controller/TecnologyController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.br.project.gerence.model.Technology;
 import com.br.project.gerence.repository.RepositoryTecnology;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,7 +29,10 @@ public class TecnologyController {
 	}
 	
 	@PostMapping
-	public Technology save(@RequestBody Technology technology) {
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Technology create(@RequestBody Technology technology) {
 		return repository.save(technology);
+//				.status(HttpStatus.CREATED)
+//				.body(repository.save(technology));
 	}
 }
