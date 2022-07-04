c2ccffd2a0aff8f32420f907ecee13408320ddc8
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-07-04 12:38:03 -0300

    Criando metodo de insercao no banco de dados

--- src/main/java/com/br/project/gerence/controller/TecnologyController.java
+++ src/main/java/com/br/project/gerence/controller/TecnologyController.java
@@ -6,6 +6,8 @@ import com.br.project.gerence.model.Technology;
 import com.br.project.gerence.repository.RepositoryTecnology;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class TecnologyController {
 	@GetMapping
 	public List<Technology> list(){
 		return repository.findAll();
+	}
+	
+	@PostMapping
+	public Technology save(@RequestBody Technology technology) {
+		return repository.save(technology);
 	}
 }
